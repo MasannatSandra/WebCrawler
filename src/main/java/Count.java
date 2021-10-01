7887f1b1c73f976a36376e49315ebd0b5f198c12
Sandra Masannat <43584949+MasannatSandra@users.noreply.github.com>
2021-09-30 23:12:17 -0700

    Improved Count.java
    Sorted frequency in order, ignored lowercase, added skeleton code to read multiple files.

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -1,8 +1,16 @@
+package count;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Count
 {
@@ -11,18 +19,38 @@ public class Count
         Scanner file = new Scanner (new File(filename));
         while(file.hasNext()){
             String word = file.next();
-            Integer count = words.get(word);
+            
+            Integer count = words.get(word.replaceAll("\\p{Punct}","").toLowerCase());
             if(count != null)
                 count++;
             else
                 count = 1;
-            words.put(word, count);
+            words = words.entrySet().stream()
+            .sorted(Comparator.comparingInt(e -> e.getValue()))
+            .collect(Collectors.toMap((entry) -> entry.getKey(), (entry) -> entry.getValue(),
+                (a, b) -> { throw new AssertionError(); },
+                LinkedHashMap::new
+        ));
+
+        words.entrySet().forEach(System.out::println);
+        words.put(word.toLowerCase(), count);
         }
         file.close();
     }
     public static void main(String[] args) throws FileNotFoundException {
         Map<String,Integer> words=new HashMap<String, Integer>();
-        CountWords("/Users/jonathanhalim/Documents/text - en/2.txt",words);
+        
+        File folder = new File("text - en.zip");
+        File[] listOfFiles = folder.listFiles();
+        
+        for (int i = 0; i < listOfFiles.length; i++) {
+            File file = listOfFiles[i];
+            if (file.isFile() && file.getName().endsWith(".txt")) {
+                //FileUtils?
+                String content = FileUtils.readFileToString(file);
+        }
+            //replace 2.txt with text - en.zip
+        CountWords("2.txt",words);
         System.out.println(words);
     }
-}
+} 
